67d7a50bbbe8d8db2dbc11c81d5c240d64a360dd
danilo <danilo@assistatecnologia.com.br>
2024-05-03 15:33:10 -0300

    feat: Deplow AWS Ec2 with provisioners

---
+++ aws_ec2_provisioners/vm.tf
@@ -0,0 +1,46 @@
+resource "aws_key_pair" "key" {
+  key_name   = "aws-key"
+  public_key = file("./aws-key.pub")
+}
+
+resource "aws_instance" "vm" {
+  ami                         = var.instance_ami
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.key.key_name
+  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  associate_public_ip_address = true
+
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> public_ip.txt"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./aws-key")
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo subnet_id: ${data.terraform_remote_state.vpc.outputs.subnet_id} >> /tmp/network_info.txt",
+      "echo security_group_id: ${data.terraform_remote_state.vpc.outputs.security_group_id} >> /tmp/network_info.txt"
+    ]
+  }
+
+  provisioner "file" {
+    source      = "./test.txt"
+    destination = "/tmp/test.txt"
+  }
+
+  provisioner "file" {
+    content     = "ami used: ${self.ami}"
+    destination = "/tmp/ami.txt"
+  }
+
+  tags = {
+    Name = "aws-vm-terraform"
+  }
+}
